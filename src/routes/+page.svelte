f478e853447f977873d249dc8c263006c4efc2f2
not.skelly <62143511+notskelly0@users.noreply.github.com>
2025-11-12 16:56:59 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <main class="flex flex-col min-h-screen bg-gray-900 text-white">
     <div class="flex-grow flex items-center justify-center py-8">
         <div class="max-w-4xl p-8 w-full">
-            <h1 class="text-5xl font-bold mb-10 text-center">Hogyan legyél ingyenélő</h1>
+            <h1 class="text-5xl font-bold mb-10 text-center">ez vajon mi lehet :)</h1>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
                 <div class="bg-gray-800 rounded-lg p-6 hover:bg-gray-700 transition-all shadow-xl flex flex-col">
                     <h2 class="text-2xl font-bold mb-4 text-blue-400">Ingyen Mobilnet</h2>
@@ -18,11 +18,11 @@
                 </div>
                 
                 <div class="bg-gray-800 rounded-lg p-6 hover:bg-gray-700 transition-all shadow-xl flex flex-col">
-                    <h2 class="text-2xl font-bold mb-4 text-green-400">V2Ray V2mess Szerver Hostolás</h2>
-                    <p class="text-gray-300 mb-4">Saját V2Ray szerver létrehozásának lépései és konfiguráció.</p>
+                    <h2 class="text-2xl font-bold mb-4 text-green-400">Saját szerver hosztolás</h2>
+                    <p class="text-gray-300 mb-4">Ehhez a részéhez sajnos nem értek úgyhogy google a barátod. -skelly</p>
                     <div class="mt-auto">
                         <button type="button" disabled class="inline-block bg-gray-600 cursor-not-allowed text-gray-400 font-semibold py-2 px-4 rounded transition-colors">
-                            Hamarosan
+                            Nem hamarosan
                         </button>
                     </div>
                 </div>
